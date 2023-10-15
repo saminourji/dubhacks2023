461a03e0572e9007790561c125a04e1947bcba27
Sami <90010511+saminourji@users.noreply.github.com>
2023-10-15 14:06:23 +0000

    Started parsing json output into list of articles

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from apikeys import TAVILY_APIKEY, OPENAI_APIKEY
+from article_selection import get_five_articles
 # from scraper import get_body_text_from_urls
 import requests
 import os
@@ -55,13 +56,13 @@ links = []
 topic_summary = tsummary.json()["answer"].split("\nSource")[0]
 messages = [{
       "role": "system",
-      "content": "You are a professional news interpreter and political scientist. You will perform two tasks.\nWhat I will send:\n1. a query in the format \\\"search topic = [TOPIC, TOPIC SUMMARY]\\\". \n2. queries in the format \\\"title = [TITLE], url = [URL], article = [ARTICLE]\\\". [TITLE] is the title of the article, [URL] is the article's url, and [ARTICLE] contains raw texts from an article about [TOPIC].\nWhat you will do:\nUsing 1., You will generate the two main points of view (pov) on [TOPIC] based on the given description [TOPIC SUMMARY]. Then, for each [ARTICLE], you will give a descriptive summary, get the media outlet name, and rank the article on a scale of FIVE levels of opinions on [TOPIC], 1 being the first main point of view, 3 being a balanced point of view, and 5 being the second main point of view. \nSummarize all this information in a JSON format using this template with propper formatting:\n{\n  \"povs\": [\n    {\"pov_title\": \"POV1 TITLE\", \"description\": \"POV1 DESCRIPTION\"},\n    {\"pov_title\": \"POV2 TITLE\", \"description\": \"POV2 DESCRIPTION\"}\n  ],\n  \"scale\": [\n    {\"level_title\": \"LEVEL1 TITLE\", \"description\": \"LEVEL1 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL2 TITLE\", \"description\": \"LEVEL2 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL3 TITLE\", \"description\": \"LEVEL3 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL4 TITLE\", \"description\": \"LEVEL4 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL5 TITLE\", \"description\": \"LEVEL5 DESCRIPTION\"}\n  ],\n  \"articles\": [\n    {\"title\": \"ARTICLE TITLE\", \"url\": \"ARTICLE URL\", \"media\": \"MEDIA NAME\", \"summary\": \"ARTICLE SUMMARY\", \"level\": \"SCALE LEVEL NUMBER\"},\n    // ... (for each article)\n  ]\n}"
+      "content": "You are a professional news interpreter and political scientist. You will perform two tasks.\nWhat I will send:\n1. a query in the format \\\"search topic = [TOPIC, TOPIC SUMMARY]\\\". \n2. queries in the format \\\"title = [TITLE], url = [URL], article = [ARTICLE]\\\". [TITLE] is the title of the article, [URL] is the article's url, and [ARTICLE] contains raw texts from an article about [TOPIC].\nWhat you will do:\nUsing 1., You will generate the two main points of view (pov) on [TOPIC] based on the given description [TOPIC SUMMARY]. Then, for each [ARTICLE], you will give a descriptive summary, get the media outlet name, and rank the article on a scale of FIVE levels of opinions on [TOPIC], 1 being the first main point of view, 3 being a balanced point of view, and 5 being the second main point of view. \nSummarize all this information in a JSON format using this template with propper formatting:\n{\n  \"povs\": [\n    {\"pov_title\": \"POV1 TITLE\", \"description\": \"POV1 DESCRIPTION\"},\n    {\"pov_title\": \"POV2 TITLE\", \"description\": \"POV2 DESCRIPTION\"}\n  ],\n  \"scale\": [\n    {\"level_title\": \"LEVEL1 NUMBER AND TITLE\", \"description\": \"LEVEL1 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL2 NUMBER AND TITLE\", \"description\": \"LEVEL2 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL3 NUMBER AND TITLE\", \"description\": \"LEVEL3 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL4 NUMBER AND TITLE\", \"description\": \"LEVEL4 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL5 NUMBER AND TITLE\", \"description\": \"LEVEL5 DESCRIPTION\"}\n  ],\n  \"articles\": [\n    {\"title\": \"ARTICLE TITLE\", \"url\": \"ARTICLE URL\", \"media\": \"MEDIA NAME\", \"summary\": \"ARTICLE SUMMARY\", \"level\": \"SCALE LEVEL NUMBER\"},\n    // ... (for each article)\n  ]\n}"
     },
     {
     "role": "user",
     "content": f"search topic = {topic}, {topic_summary}"
 }]
-cap = 2700
+cap = 2650
 if articles.status_code != 200:
     print(f"Error: {articles.status_code}")
     print(articles.text)
@@ -96,4 +97,6 @@ response = openai.ChatCompletion.create(
     frequency_penalty=0,
     presence_penalty=0
 )
-print(response['choices'][0]['message']['content'])
+data = response['choices'][0]['message']['content']
+json_data = json.loads(data)
+print(get_five_articles(json_data))
