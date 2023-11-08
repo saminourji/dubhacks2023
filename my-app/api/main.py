6ec68a24144d90b8acf6185334c888134dce193f
Sami <90010511+saminourji@users.noreply.github.com>
2023-11-08 00:14:02 +0000

    Unpublishd

--- my-app/api/main.py
+++ my-app/api/main.py
@@ -2,18 +2,21 @@ from flask import Flask
 
 
 import json
-import requests
 import os
+import requests
 import openai
 import re
 from apikeys import TAVILY_APIKEY, OPENAI_APIKEY
 from article_selection import get_five_articles
+
+
 # from scraper import get_body_text_from_urls
+os.environ["OPENAI_API_KEY"] = "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"
+openai.api_key = "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"
 
+
+from flask import Flask
 app = Flask(__name__)
-
-os.environ["OPENAI_API_KEY"] = OPENAI_APIKEY
-openai.api_key = OPENAI_APIKEY
 
 @app.route('/get_text')
 def get_text():
@@ -46,105 +49,173 @@ def get_text():
     topic_summary = tsummary.json()["answer"].split("\nSource")[0]
     return topic_summary
 
-@app.route('/get_articles')
-def get_articles():
-    # Base URL and endpoint
-    base_url = "https://api.tavily.com/"
-    endpoint = "search"
+# @app.route('/get_articles')
+# def articles():
+#     # Base URL and endpoint
+#     base_url = "https://api.tavily.com/"
+#     endpoint = "search"
 
-    topic = "Israel Palestine Conflict"
+#     topic = "iSRAEL pALESTINE CONFLICT"
 
-    tsummary_payload = {
-        "query": f"Explain the topic '{topic}' by giving past history, and providing context on recent events.",
-        "search_depth": "basic",
-        "include_answer": True,
-        "include_raw_content": False,
-        "exclude_domains": ["wikipedia.com", "apnews.com/hub"],
-        "max_results": 10,
-        "api_key": TAVILY_APIKEY
-    }
+#     tsummary_payload = {
+#         "query": f"Explain the topic '{topic}' by giving past history, and providing context on recent events.",
+#         "search_depth": "basic",
+#         "include_answer": True,
+#         "include_raw_content": False,
+#         "exclude_domains": ["wikipedia.com", "apnews.com/hub"],
+#         "max_results": 10,
+#         "api_key": TAVILY_APIKEY
+#     }
 
-    article_payload = {
-        "query": f"Give news articles discussing the topic '{topic}' with a wide range of perspectives. No government websites. Do not include a media outlet domain as a source more than once",
-        "search_depth": "basic",
-        "include_answer": False,
-        "include_raw_content": True,
-        "exclude_domains": ["wikipedia.com", "apnews.com/hub"],
-        "max_results": 10,
-        "api_key": TAVILY_APIKEY
-    }
+#     article_payload = {
+#         "query": f"Give news articles discussing the topic '{topic}' with a wide range of perspectives. No government websites. Do not include a media outlet domain as a source more than once",
+#         "search_depth": "basic",
+#         "include_answer": False,
+#         "include_raw_content": True,
+#         "exclude_domains": ["wikipedia.com", "apnews.com/hub"],
+#         "max_results": 10,
+#         "api_key": TAVILY_APIKEY
+#     }
 
+#     article_payload_json = json.dumps(article_payload)
+#     tsummary_payload_json = json.dumps(tsummary_payload)
 
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+
+#     print("Getting topic summary")
+#     tsummary = requests.post(f"{base_url}{endpoint}",
+#                             data=tsummary_payload_json, headers=headers)
+
+#     print("Getting articles on topic")
+#     articles = requests.post(f"{base_url}{endpoint}",
+#                             data=article_payload_json, headers=headers)
+
+#     result_list = []
+#     links = []
+#     topic_summary = tsummary.json()["answer"].split("\nSource")[0]
     
 
-    article_payload_json = json.dumps(article_payload)
-    tsummary_payload_json = json.dumps(tsummary_payload)
+#     messages = [{
+#         "role": "system",
+#         "content": "You are a professional news interpreter and political scientist. You will perform two tasks.\nWhat I will send:\n1. a query in the format \\\"search topic = [TOPIC, TOPIC SUMMARY]\\\". \n2. queries in the format \\\"title = [TITLE], url = [URL], article = [ARTICLE]\\\". [TITLE] is the title of the article, [URL] is the article's url, and [ARTICLE] contains raw texts from an article about [TOPIC].\nWhat you will do:\nUsing 1., You will generate the two main points of view (pov) on [TOPIC] based on the given description [TOPIC SUMMARY]. Then, for each [ARTICLE], you will give a descriptive summary, get the media outlet name, and rank the article on a scale of FIVE levels of opinions on [TOPIC], 1 being the first main point of view, 3 being a balanced point of view, and 5 being the second main point of view. \nSummarize all this information in a JSON format using this template with propper formatting:\n{\n  \"povs\": [\n    {\"pov_title\": \"POV1 TITLE\", \"description\": \"POV1 DESCRIPTION\"},\n    {\"pov_title\": \"POV2 TITLE\", \"description\": \"POV2 DESCRIPTION\"}\n  ],\n  \"scale\": [\n    {\"level_title\": \"LEVEL1 NUMBER AND TITLE\", \"description\": \"LEVEL1 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL2 NUMBER AND TITLE\", \"description\": \"LEVEL2 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL3 NUMBER AND TITLE\", \"description\": \"LEVEL3 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL4 NUMBER AND TITLE\", \"description\": \"LEVEL4 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL5 NUMBER AND TITLE\", \"description\": \"LEVEL5 DESCRIPTION\"}\n  ],\n  \"articles\": [\n    {\"title\": \"ARTICLE TITLE\", \"url\": \"ARTICLE URL\", \"media\": \"MEDIA NAME\", \"summary\": \"ARTICLE SUMMARY\", \"level\": \"SCALE LEVEL NUMBER\"},\n    // ... (for each article)\n  ]\n}"
+#         },
+#         {
+#         "role": "user",
+#         "content": f"search topic = {topic}, {topic_summary}"
+#     }]
+#     cap = 2650
+#     if articles.status_code != 200:
+#         print(f"Error: {articles.status_code}")
+#         print(articles.text)
+#     else:
+#         search_results = articles.json()
+#         results = search_results["results"]
+#         # print(results)
+#         i = 0 
+#         for result in results:
+#             data = (result["title"], result["url"], result["raw_content"])
+#             # print(data)
+#             result_list.append(data)
+#             if (len(data[2]) >= cap):
+#                 messages.append({
+#                     "role": "user",
+#                     "content": f"title = {data[0]}, url = {data[1]}, article = {data[2][:cap]}"
+#                 })
+#             else:
+#                 messages.append({
+#                     "role": "user",
+#                     "content": f"title = {data[0]}, url = {data[1]}, article = {data[2]}"
+#                 })
+#             # print(f"nb of articles: {i}")
 
-    headers = {
-        "Content-Type": "application/json"
-    }
+#     print("startGPT")
+#     response = openai.ChatCompletion.create(
+#         model="gpt-4",
+#         messages=messages,
+#         temperature=1,
+#         max_tokens=2049,
+#         top_p=1,
+#         frequency_penalty=0,
+#         presence_penalty=0
+#     )
+#     print("got response")
+#     data = response['choices'][0]['message']['content']
+#     json_data = json.loads(data)
+#     data = json_data
+#     print("DONE")
 
-    print("Getting topic summary")
-    tsummary = requests.post(f"{base_url}{endpoint}",
-                            data=tsummary_payload_json, headers=headers)
+#     i = 0
+#     medias = []
+#     levels = []
+#     selected = []
+#     #preliminary selection of unique (media, level) pairs
+#     for article in data["articles"]:
+#         media = article["media"]
+#         match = re.search(r'\d+', article["level"])
+#         if match:
+#           article["level"] = match.group()
+#         level = int(article["level"])
+#         # level = int(article["level"])
+#         if media not in medias or level not in levels:
+#             print(f"Just included: {media}: {media in medias}, {level}: {level in levels}")
+#             selected.append(article)
+#             levels.append(level) 
+#             medias.append(media)
+#         else:
+#             print(f"Already included: {media}: {media in medias}, {level}: {level in levels}")
 
-    print("Getting articles on topic")
-    articles = requests.post(f"{base_url}{endpoint}",
-                            data=article_payload_json, headers=headers)
+#     five_selections1 = []
+#     selected_medias1 = []
+#     # five_selections2 = []
+#     # selected_medias2 = []
+#     #for each level, include first article pair, none if nothing; no duplicate medias
+#     for i in range(1,6):
+#         # print(levels)
+#         if i in levels:
+#             position = levels.index(i)
+#             while (medias[position] in selected_medias1 and (position != len(levels)-1 and i in levels[position+1:])):
+#                 position = levels[position+1:].index(i)
+            
+#             # if (position == len(levels)-1 or i not in levels[position+1:]):
+#             #     five_selections1.append(None)
+#             #     print(f"{i} not levels")
+#             # else:
+#             five_selections1.append(selected[position])
+#             selected_medias1.append(selected[position]['media'])
+#             print(f"Included media {selected[position]['media']} for level {selected[position]['level']}")
 
-    result_list = []
-    links = []
-    topic_summary = tsummary.json()["answer"].split("\nSource")[0]
-    
+#         else:
+#             five_selections1.append({"title": "None", "url": "", "media": "None", "summary": "", "level": "3"})
+#             print(f"{i} not levels")
 
-    messages = [{
-        "role": "system",
-        "content": "You are a professional news interpreter and political scientist. You will perform two tasks.\nWhat I will send:\n1. a query in the format \\\"search topic = [TOPIC, TOPIC SUMMARY]\\\". \n2. queries in the format \\\"title = [TITLE], url = [URL], article = [ARTICLE]\\\". [TITLE] is the title of the article, [URL] is the article's url, and [ARTICLE] contains raw texts from an article about [TOPIC].\nWhat you will do:\nUsing 1., You will generate the two main points of view (pov) on [TOPIC] based on the given description [TOPIC SUMMARY]. Then, for each [ARTICLE], you will give a descriptive summary, get the media outlet name, and rank the article on a scale of FIVE levels of opinions on [TOPIC], 1 being the first main point of view, 3 being a balanced point of view, and 5 being the second main point of view. \nSummarize all this information in a JSON format using this template with propper formatting:\n{\n  \"povs\": [\n    {\"pov_title\": \"POV1 TITLE\", \"description\": \"POV1 DESCRIPTION\"},\n    {\"pov_title\": \"POV2 TITLE\", \"description\": \"POV2 DESCRIPTION\"}\n  ],\n  \"scale\": [\n    {\"level_title\": \"LEVEL1 NUMBER AND TITLE\", \"description\": \"LEVEL1 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL2 NUMBER AND TITLE\", \"description\": \"LEVEL2 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL3 NUMBER AND TITLE\", \"description\": \"LEVEL3 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL4 NUMBER AND TITLE\", \"description\": \"LEVEL4 DESCRIPTION\"},\n    {\"level_title\": \"LEVEL5 NUMBER AND TITLE\", \"description\": \"LEVEL5 DESCRIPTION\"}\n  ],\n  \"articles\": [\n    {\"title\": \"ARTICLE TITLE\", \"url\": \"ARTICLE URL\", \"media\": \"MEDIA NAME\", \"summary\": \"ARTICLE SUMMARY\", \"level\": \"SCALE LEVEL NUMBER\"},\n    // ... (for each article)\n  ]\n}"
-        },
-        {
-        "role": "user",
-        "content": f"search topic = {topic}, {topic_summary}"
-    }]
-    cap = 2650
-    if articles.status_code != 200:
-        print(f"Error: {articles.status_code}")
-        print(articles.text)
-    else:
-        search_results = articles.json()
-        results = search_results["results"]
-        # print(results)
-        i = 0 
-        for result in results:
-            data = (result["title"], result["url"], result["raw_content"])
-            # print(data)
-            result_list.append(data)
-            if (len(data[2]) >= cap):
-                messages.append({
-                    "role": "user",
-                    "content": f"title = {data[0]}, url = {data[1]}, article = {data[2][:cap]}"
-                })
-            else:
-                messages.append({
-                    "role": "user",
-                    "content": f"title = {data[0]}, url = {data[1]}, article = {data[2]}"
-                })
-            # print(f"nb of articles: {i}")
+#     # for i in range(5, 0, -1):
+#     #     if i in levels:
+#     #         position = levels.index(i)
+#     #         while (
+#     #             selected[position]["media"] in selected_medias2
+#     #             and (position != len(levels) - 1 and i in levels[position + 1 :])
+#     #         ):
+#     #             position = levels[position + 1 :].index(i)
+
+#     #         five_selections2.append(selected[position])
+#     #         selected_medias2.append(selected[position]["media"])
+#     #         print(f"Included media {selected[position]['media']} for level {selected[position]['level']}")
+#     #     else:
+#     #         five_selections2.append(None)
+#     #         print(f"{i} not in levels")
+
+#     # print(selected_medias1)
+#     # print(selected_medias2)
+
+#     # if (len(list(set(selected_medias1))) >= len(list(set(selected_medias2)))):
+        
+#     return five_selections1
 
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=messages,
-        temperature=1,
-        max_tokens=2049,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    data = response['choices'][0]['message']['content']
-    json_data = json.loads(data)
-    five_articles = get_five_articles(json_data)
-    print("DONE")
-    return five_articles
+    # return five_articles
 
 if (__name__ == '__main__'):
     app.run(debug=True)
+# print(articles())
--- my-app/src/components/about/main.js
+++ my-app/src/components/about/main.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./main.css";
 import nbcImage from "../../assets/334_nbc.jpg"; // Corrected import path to your local image file
 import nytImage from "../../assets/nyt.png";
@@ -8,7 +8,7 @@ import cnnImage from "../../assets/CNN_International_logo.svg.png";
 // import TextDisplay from './TextDisplay';
 
 const Main = (props) => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [articles, setArticles] = useState([]);
   const [title, setTitle] = useState("Quick Summary");
   const [title1, setTitle1] = useState("Loading...");
@@ -17,125 +17,133 @@ const Main = (props) => {
   const [title4, setTitle4] = useState("Loading...");
   const [title5, setTitle5] = useState("Loading...");
 
+  const [hasRun, setHasRun] = useState(false);
+
   useEffect(() => {
-    // Make an API request to the backend
-    fetch('/get_text') // Update with the actual backend URL
-      .then((response) => (response.text()))
-      .then((data) => setText(data))
-      .catch((error) => console.error('Error:', error));
-  
-    fetch('/get_articles') // Update with the actual backend URL
-      // .then((response) => (response))
-      .then((data) => { setArticles(data)
-      setTitle1(data[0]["media"])
-      setTitle2(data[1]["media"])
-      setTitle3(data[2]["media"])
-      setTitle4(data[3]["media"])
-      setTitle5(data[4]["media"])
-    })
-      .catch((error) => console.error('Error:', error));
-    }, []);
+    // if (!hasRun) {
+      // Make an API request to the backend
+      fetch('/get_text') // Update with the actual backend URL
+        .then((response) => (response.text()))
+        .then((data) => setText(data))
+        .catch((error) => console.error('Error:', error));
+      // fetch('/get_articles') // Update with the actual backend URL
+      //   // .then((response) => (response))
+      //   .then((data) => {
+      //     setArticles(data)
+      //     setTitle1(data[0]["media"])
+      //     setTitle2(data[1]["media"])
+      //     setTitle3(data[2]["media"])
+      //     setTitle4(data[3]["media"])
+      //     setTitle5(data[4]["media"])
+      //   })
+      //   .catch((error) => console.error('Error:', error));
+      
+      //   setHasRun(true)
+    // }
+  }, []);
 
   const [selectedImage, setSelectedImage] = useState(null);
 
   const handleImageClick = (imageName) => {
-    setSelectedImage(imageName);
-    setText(articles[Number(selectedImage)-1]["summary"])
-    setTitle(articles[Number(selectedImage)-1]["title"])
+    if (hasRun) {
+      setSelectedImage(imageName);
+      setText(articles[Number(selectedImage) - 1]["summary"])
+      setTitle(articles[Number(selectedImage) - 1]["title"])
+    }
   };
 
   return (
-      <div className="main">
-        <div className="buttonContainer">
-          <div className="buttonText">Back</div>
-        </div>
-        <div className="mainTitle">Israel and Palestine Conflict</div>
-        <div className="timelineContainer">
-          <ul class="timeline">
-            <li>
-              <div className="tooltip">
-                <div className="newsOutletContainer">
-                  <div className="newsOutletImage">
-                    <img 
-                      onClick={() => handleImageClick('1')} 
-                      src={washingtonImage}
-                      alt="Washington Times"
-                      className="image"
-                    />
-                  </div>
-                  <div className="newsOutletText">{title1}</div>
+    <div className="main">
+      <div className="buttonContainer">
+        <div className="buttonText">Back</div>
+      </div>
+      <div className="mainTitle">Israel and Palestine Conflict</div>
+      <div className="timelineContainer">
+        <ul class="timeline">
+          <li>
+            <div className="tooltip">
+              <div className="newsOutletContainer">
+                <div className="newsOutletImage">
+                  <img
+                    onClick={() => handleImageClick('1')}
+                    src={washingtonImage}
+                    alt="Washington Times"
+                    className="image"
+                  />
                 </div>
+                <div className="newsOutletText">{title1}</div>
               </div>
-            </li>
-            <li>
-              <div className="tooltip">
-                <div className="newsOutletContainer">
-                  <div className="newsOutletImage">
-                    <img 
-                      onClick={() => handleImageClick('2')} 
-                      src={nytImage}
-                      alt="New York Times"
-                      className="image"
-                    />
-                  </div>
-                  <div className="newsOutletText">{title2}</div>
+            </div>
+          </li>
+          <li>
+            <div className="tooltip">
+              <div className="newsOutletContainer">
+                <div className="newsOutletImage">
+                  <img
+                    onClick={() => handleImageClick('2')}
+                    src={nytImage}
+                    alt="New York Times"
+                    className="image"
+                  />
                 </div>
+                <div className="newsOutletText">{title2}</div>
               </div>
-            </li>
-            <li>
-              <div className="tooltip">
-                <div className="newsOutletContainer">
-                  <div className="newsOutletImage">
-                    <img onClick={() => handleImageClick('3')} 
-                      src={nbcImage}
-                      alt="Washington Times"
-                      className="image"
-                    />
-                  </div>
-                  <div className="newsOutletText">{title3}</div>
+            </div>
+          </li>
+          <li>
+            <div className="tooltip">
+              <div className="newsOutletContainer">
+                <div className="newsOutletImage">
+                  <img onClick={() => handleImageClick('3')}
+                    src={nbcImage}
+                    alt="Washington Times"
+                    className="image"
+                  />
                 </div>
+                <div className="newsOutletText">{title3}</div>
               </div>
-            </li>
-            <li>
-              <div className="tooltip">
-                <div className="newsOutletContainer">
-                  <div className="newsOutletImage">
-                    <img 
-                      onClick={() => handleImageClick('4')} 
-                      src={foxImage}
-                      alt="Washington Times"
-                      className="image"
-                    />
-                  </div>
-                  <div className="newsOutletText">{title4}</div>
+            </div>
+          </li>
+          <li>
+            <div className="tooltip">
+              <div className="newsOutletContainer">
+                <div className="newsOutletImage">
+                  <img
+                    onClick={() => handleImageClick('4')}
+                    src={foxImage}
+                    alt="Washington Times"
+                    className="image"
+                  />
                 </div>
+                <div className="newsOutletText">{title4}</div>
               </div>
-            </li>
-            <li>
-              <div className="tooltip">
-                <div className="newsOutletContainer">
-                  <div className="newsOutletImage">
-                    <img 
-                      onClick={() => handleImageClick('5')} 
-                      src={cnnImage}
-                      alt="Washington Times"
-                      className="image"
-                    />
-                  </div>
-                  <div className="newsOutletText">{title5}</div>
+            </div>
+          </li>
+          <li>
+            <div className="tooltip">
+              <div className="newsOutletContainer">
+                <div className="newsOutletImage">
+                  <img
+                    onClick={() => handleImageClick('5')}
+                    src={cnnImage}
+                    alt="Washington Times"
+                    className="image"
+                  />
                 </div>
+                <div className="newsOutletText">{title5}</div>
               </div>
-            </li>
-          </ul>
-        </div>
+            </div>
+          </li>
+        </ul>
+      </div>
 
-        <div className="summaryContainer">
-          <div className="summaryTitle">{title}</div>
-          <div className="summaryText">
-            {text}
-          </div>
+      <div className="summaryContainer">
+        <div className="summaryTitle">{title}</div>
+        <div className="summaryText">
+          {text}
         </div>
       </div>
+    </div>
   );
 };
 
